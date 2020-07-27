dcde68a1b16be14ae0cde93e47320ba79de38f2a
Leonel <dismoi.leo@gmail.com>
2020-07-27 02:23:02 -0300

    'commit'

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ require('./models/Nodo');
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/Huertas-Ant`);
+mongoose.connect(`mongodb+srv://Usuario-Administrador:60pVzoUn9uRtsUch@cluster0.snph0.mongodb.net/Huertas-Ant?retryWrites=true&w=majority` || `mongodb://localhost:27017/Huertas-Ant`);
 //mongoose.connect(process.env.MONGODB_URI || process.env.DB_URL);
 app.use(bodyParser.json());
 
