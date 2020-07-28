e4d01f41f1375ba5f0237651d65819b3b53f8350
Leonel <dismoi.leo@gmail.com>
2020-07-28 00:36:53 -0300

    'commint'

--- index.js
+++ index.js
@@ -15,8 +15,20 @@ require('./models/Nodo');
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI  || `mongodb://localhost:27017/Huertas-Ant`);
+//mongoose.connect(process.env.MONGODB_URI  || `mongodb://localhost:27017/Huertas-Ant`);
 //mongoose.connect(process.env.MONGODB_URI || process.env.DB_URL);
+
+//pruebaa
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://Usuario-Administrador:60pVzoUn9uRtsUch@cluster0.snph0.mongodb.net/Huertas-Ant?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("Huertas-Ant").collection("nodos");
+  // perform actions on the collection object
+  client.close();
+});
+
+//fin prueba
 app.use(bodyParser.json());
 
 //aca se importan las rutas
